00a5b00334e5e2630fdcc01618d1476d7872bf8f
tabertop <andy@gomaxrx.com>
2026-03-16 19:21:20 -0400

    fix: move GA4 to _app.js — fires on every page including /tiktok

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
+const GA_ID = 'G-T0YGHRKCQ6'
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        {/* GA4 — loads on every page including /tiktok */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}></script>
+        <script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
